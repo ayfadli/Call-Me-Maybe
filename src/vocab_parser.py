8fd5ef3974cf1fe0e6ab2451d5e55c0169639bae
Ayoub Fadli <ayfadli@c1r7p4.1337.ma>
2026-04-27 19:50:17 +0100

    Auto-save before logout: 2026-04-27 19:50

--- src/vocab_parser.py
+++ src/vocab_parser.py
@@ -1,6 +1,22 @@
 from llm_sdk import Small_LLM_Model
 import json
 import sys
+import string
+
+def get_allowed_chars(current_string):
+    # The static part of the JSON that never changes
+    static_prefix = '{"name": "'
+
+    if  static_prefix.startswith(current_string) and current_string != static_prefix:
+            next_required_char = static_prefix[len(current_string)]
+            return next_required_char
+
+    elif current_string.startswith(static_prefix):
+        allowed = list(string.ascii_lowercase) + ['_', '"']
+        return allowed
+
+    return []
+
 
 def main():
     model = Small_LLM_Model()
@@ -20,7 +36,70 @@ def main():
 
    #Reverse (map) the key and value pairs in a dict:
     my_dict = {v: k.replace('Ġ', ' ') for k, v in vocab_data.items()}
-    print(my_dict[9625])
+
+    prompt = "The capital of France is"
+
+    input_ids = model.encode(prompt)
+
+    # --- THE FIX: Sanitize the IDs into a flat Python list ---
+    # 1. If encode() returned a PyTorch tensor, convert it to a Python list
+    if hasattr(input_ids, 'tolist'):
+        input_ids = input_ids.tolist()
+
+    # 2. If encode() returned a 2D list (e.g., [[785, 6722]]), flatten it to 1D
+    if len(input_ids) > 0 and isinstance(input_ids[0], list):
+        input_ids = input_ids[0]
+
+    generated_json = ""
+
+    logits = model.get_logits_from_input_ids(input_ids)
+
+    print("--- Forcing JSON Output ---")
+    while not generated_json.endswith('}'):
+
+        logits = model.get_logits_from_input_ids(input_ids)
+        allowed_chars = get_allowed_chars(generated_json)
+
+        for token_id in range(len(logits)):
+
+            token_str = my_dict.get(token_id)
+
+            if token_str is None:
+                logits[token_id] = float('-inf')
+                continue
+
+            if len(allowed_chars) > 0:
+
+                is_valid = False
+
+                clean_token = token_str.lstrip()
+
+                for allowed in allowed_chars:
+                    if clean_token.startswith(allowed):
+                        is_valid = True
+                        break
+
+                if not is_valid:
+                    logits[token_id] = float('-inf')
+
+        best_token_id = logits.index(max(logits))
+        winning_str = my_dict[best_token_id]
+
+        if generated_json == "":
+            winning_str = winning_str.lstrip()
+
+        generated_json += winning_str
+        input_ids.append(best_token_id)
+
+        print(f"\rCurrent Output: {generated_json}", end="", flush=True)
+
+        if len(generated_json) > 50:
+            print("\n[Safety Break] String got too long without closing!")
+            break
+
+
+    print(f"\n\nFinal JSON: {generated_json}")
+
 
 if __name__ == "__main__":
     main()
